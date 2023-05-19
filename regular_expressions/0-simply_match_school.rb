c477276add9f22fc260e7ef4d58f40df8df62f4a
KurMalual <k.malual@alustudent.com>
2023-05-19 03:22:31 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts ARGV [0].scan(/School/).join
+#Get the argument from the command line
+input = ARGV[0]
+
+# Define the regular expreeion
+pattern = /School/
+
+#Check if the input matches the pattern
+if input =~ pattern
+  puts "School"
+else
+  puts "SchoolSchool"
+end
+puts ARGV[0].scan(/School).join
