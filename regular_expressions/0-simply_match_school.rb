212bb740e5c4cdf409d7113a45265d58c40a9d96
KurMalual <k.malual@alustudent.com>
2023-05-19 03:25:29 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-#Get the argument from the command line
+# Get the argument from the command line
 input = ARGV[0]
 
-# Define the regular expreeion
+# Define the regular expression
 pattern = /School/
 
-#Check if the input matches the pattern
+# Check if the input matches the pattern
 if input =~ pattern
   puts "School"
 else
